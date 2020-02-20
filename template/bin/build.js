2bd3fbd0359b636102f5ec6b382b3ccccccf3b3b
wangweihua <wangweihua@vipkid.com.cn>
2020-02-20 20:56:25 +0800

    :memo: (bin/build.js) add cocos creator cmd line build options link

--- template/bin/build.js
+++ template/bin/build.js
@@ -1,4 +1,5 @@
 const utils = require('./utils.js')
+// http://docs.cocos.com/creator/manual/zh/publish/publish-in-command-line.html
 utils.cocos(`--path ${utils.projectDir} --build ${[
   'debug=false',
   'platform=web-mobile',
