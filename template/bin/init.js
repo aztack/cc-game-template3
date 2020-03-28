c51c7eaaa01b69453ec8e17c755b02128f6aca7b
wangweihua <wangweihua@vipkid.com.cn>
2020-03-28 22:50:11 +0800

    :bug: (bin/init.js) create skeletons folder; do not create scripts/cc_modules

--- template/bin/init.js
+++ template/bin/init.js
@@ -6,7 +6,7 @@ const srcDir = path.resolve(__dirname, '../src');
 const newDir = path.resolve(__dirname, `../${projectDir}`);
 console.log(`rename source code folder to ${newDir}`.green.bold);
 shell.mv(srcDir, newDir);
-['scripts/cc_modules', 'animations', 'audios', 'scenes'].forEach(function(dir){
+['scripts', 'animations', 'audios', 'scenes', 'skeletons'].forEach(function(dir){
   console.log(`creating ${dir} folder`);
   shell.mkdir('-p', `${newDir}/assets/${dir}`);
 });
