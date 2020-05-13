721c98bba2f6fc9c3806d3b60ba4b7ee523b3ded
wangweihua <wangweihua@vipkid.com.cn>
2020-05-13 18:55:58 +0800

    remove build params in build.js

--- template/bin/build.js
+++ template/bin/build.js
@@ -1,9 +1,3 @@
 const utils = require('./utils.js')
 // http://docs.cocos.com/creator/manual/zh/publish/publish-in-command-line.html
-utils.cocos(`--path ${utils.projectDir} --build ${[
-  'debug=false',
-  'platform=web-mobile',
-  'inlineSpriteFrames=true',
-  'md5Cache=true',
-  'mergeStartScene=true'
-].join(';')}`, true);
+utils.cocos(`--path ${utils.projectDir} --build`, true);
