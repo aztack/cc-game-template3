b1a7d9a1aec8613de2a1f65f5584b42d9915deeb
wangweihua <wangweihua@vipkid.com.cn>
2020-05-28 14:59:29 +0800

    :bug: (Main.ts) rename class from NewClass to Main

--- template/src/assets/Main.ts
+++ template/src/assets/Main.ts
@@ -1,7 +1,7 @@
 const {ccclass, property} = cc._decorator;
 
 @ccclass
-export default class NewClass extends cc.Component {
+export default class Main extends cc.Component {
   onLoad () {}
   start () {}
 }
