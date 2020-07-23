b6acf66ac0627c9371ff389e9231e90a9f176be8
wangweihua <wangweihua@vipkid.com.cn>
2020-07-23 16:57:38 +0800

    feat(build-finish.js): replace $projectId$ in main.js when build finished for cocos play

--- template/bin/build-finish.js
+++ template/bin/build-finish.js
@@ -2,6 +2,18 @@ module.exports = function(options) {
   const ejs = require('ejs');
   const fs = require('fs');
   const path = require('path');
+
+  // cocos-play
+  const mainPath = path.resolve(options.project, 'build/cocos-play/main.js');
+  if (options.actualPlatform === 'jkw-game' && fs.existsSync(mainPath)) {
+    const src = fs.readFileSync(mainPath).toString();
+    const packageJsonPath = path.resolve(options.project, '../package.json');
+    const pkg = JSON.parse(fs.readFileSync(packageJsonPath)).name;
+    fs.writeFileSync(mainPath, src.replace('$projectId$', pkg));
+    return;
+  }
+
+  // web-mobile
   const indexPath = 'build/web-mobile/index.html';
   if (!fs.existsSync(indexPath) || options.actualPlatform !== 'web-mobile') {
     Editor.log(`Build-finish: Can not find ${indexPath}, stop post-process`);
